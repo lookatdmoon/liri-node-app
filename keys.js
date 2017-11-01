ce1d67a72bed846105b0bd53f3922da6eb363adf
Mhirra Yung <lookatdmoon@gmail.com>
2017-10-31 21:23:50 -0500

    Inital commit.

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'rEAodSx9WrWkbJW1oydcHno6r',
+  consumer_secret: 'SYDCWR2KxUlZeYAxQ7Wel5XOE2o5YEG7tqftQQoVKy9bCDSjgM',
+  access_token_key: '924160425192763392-kOyghRYYlcSJU068GpYFB9ywhkMoDv9',
+  access_token_secret: 'RekB4MTH65oIOnspjIepoYxhknGRrjRR4axTVNSfbCRfp',
+}
+
+module.exports = twitterKeys;
